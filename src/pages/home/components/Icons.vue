3c7a5e1846a26c5a18dde183b9a18a06dbe6a052
会飞面包 <872877904@qq.com>
2019-08-04 12:49:43 +0800

    08-04-09-07

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -6,7 +6,7 @@
           <div class="icon-img">
             <img class="icon-img-content" :src="item.imgUrl">
           </div>
-          <p class="icon-desc">{{item.desc}}}</p>
+          <p class="icon-desc">{{item.desc}}</p>
         </div>
       </swiper-slide>
     </swiper>
